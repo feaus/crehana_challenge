7a65e2e132f49103134af1e5febbf4016bdc428e
fauster <federicoauster@gmail.com>
2022-02-15 21:16:22 -0300

    Updated main.py with integration routes and creating tables

--- src/main.py
+++ src/main.py
@@ -3,11 +3,16 @@ from pathlib import Path
 from dotenv import load_dotenv
 from fastapi import FastAPI
 
-from src.routers import third_party
+from src.routers import integration, third_party
+from src.database.database import Base, engine
+
+
+Base.metadata.create_all(bind=engine)
 
 
 load_dotenv(f'{Path().absolute()}/src/secrets/.env')
 
 app = FastAPI()
 
+app.include_router(integration.router)
 app.include_router(third_party.router)
