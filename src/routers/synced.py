aa5b2a8f8aa7f81f593fe295c872ecf824980685
fauster <federicoauster@gmail.com>
2022-02-15 21:42:23 -0300

    Created synced.py with its functions

---
+++ src/routers/synced.py
@@ -0,0 +1,32 @@
+import uuid
+
+from fastapi import APIRouter
+
+from src.controllers.posts import get_post_by_id, get_posts
+
+
+router = APIRouter(
+    prefix='/synced',
+)
+
+
+@router.get('/posts')
+def get_synced_posts():
+    stored_posts = get_posts()
+    return {
+        'message': 'Synced posts returned successfully',
+        'data': stored_posts,
+    }
+
+
+@router.get('/posts/{post_id}')
+def get_synced_posts(post_id: uuid.UUID):
+    stored_post = get_post_by_id(post_id)
+
+    # if stored_post is None:  # TODO: raise custom exception
+    #     raise Exception
+
+    return {
+        'message': 'Synced post returned successfully',
+        'data': [stored_post],
+    }
