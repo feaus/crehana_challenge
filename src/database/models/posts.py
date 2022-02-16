decc69d996c0ae861b9a546ff022272d4698b178
fauster <federicoauster@gmail.com>
2022-02-15 21:17:35 -0300

    Created models package and Posts model

---
+++ src/database/models/posts.py
@@ -0,0 +1,16 @@
+import uuid
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.dialects.postgresql import UUID
+
+from src.database.database import Base
+
+
+class Posts(Base):
+    __tablename__ = "posts"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    integration_id = Column(Integer, index=True)
+    title = Column(String)
+    body = Column(String)
+    user_id = Column(Integer)
