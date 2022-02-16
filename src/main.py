1790114a9a5c9e6eb555992820cacdf210db3ed5
fauster <federicoauster@gmail.com>
2022-02-15 21:41:58 -0300

    Added synced routes in main.py

--- src/main.py
+++ src/main.py
@@ -3,8 +3,8 @@ from pathlib import Path
 from dotenv import load_dotenv
 from fastapi import FastAPI
 
-from src.routers import integration, third_party
-from src.database.database import Base, engine
+from src.routers import integration, synced, third_party
+from src.database.connection import Base, engine
 
 
 Base.metadata.create_all(bind=engine)
@@ -15,4 +15,5 @@ load_dotenv(f'{Path().absolute()}/src/secrets/.env')
 app = FastAPI()
 
 app.include_router(integration.router)
+app.include_router(synced.router)
 app.include_router(third_party.router)
