0a8bc02fd627786f4dc6c26860a0564fa208097e
fauster <federicoauster@gmail.com>
2022-02-20 16:02:49 -0300

    Added delete mutations in query.py

--- src/schemas/query.py
+++ src/schemas/query.py
@@ -1,3 +1,5 @@
+import datetime
+
 import graphene
 
 from src.database.connection import SessionLocal
@@ -14,7 +16,6 @@ db = SessionLocal()
 
 
 class Query(graphene.ObjectType):
-    say_hello = graphene.String(name=graphene.String(default_value='Test Driven'))
     list_cars = graphene.List(CarsGraphQLModel)
     list_dealers = graphene.List(CarDealersGraphQLModel)
     get_car = graphene.Field(
@@ -27,24 +28,26 @@ class Query(graphene.ObjectType):
     )
 
     @staticmethod
-    def resolve_say_hello(parent, info, name):
-        return f'Hello {name}'
-
-    @staticmethod
     def resolve_list_cars(parent, info):
-        return db.query(Cars).all()
+        return db.query(Cars).filter(Cars.deleted_at == None).all()
 
     @staticmethod
     def resolve_list_dealers(parent, info):
-        return db.query(CarDealers).all()
+        return db.query(CarDealers).filter(CarDealers.deleted_at == None).all()
 
     @staticmethod
     def resolve_get_car(parent, info, car_id):
-        return db.query(Cars).filter(Cars.id == car_id).first()
+        return db.query(Cars).filter(
+            Cars.id == car_id,
+            Cars.deleted_at == None,
+        ).first()
 
     @staticmethod
     def resolve_get_car_dealer(parent, info, dealer_id):
-        return db.query(CarDealers).filter(CarDealers.id == dealer_id).first()
+        return db.query(CarDealers).filter(
+            CarDealers.id == dealer_id,
+            CarDealers.deleted_at == None,
+        ).first()
 
 
 class CreateCar(graphene.Mutation):
@@ -95,7 +98,13 @@ class EditCar(graphene.Mutation):
 
     @staticmethod
     def mutate(parent, info, car_id, car_details):
-        car: Cars = db.query(Cars).filter(Cars.id == car_id).first()
+        car: Cars = db.query(Cars).filter(
+            Cars.id == car_id,
+            Cars.deleted_at == None
+        ).first()
+
+        if car is None:
+            return
 
         for key, value in car_details.items():
             if value is not None:
@@ -116,7 +125,12 @@ class EditCarDealer(graphene.Mutation):
     @staticmethod
     def mutate(parent, info, dealer_id, dealer_details):
         dealer: CarDealers = db.query(CarDealers).filter(
-            CarDealers.id == dealer_id).first()
+            CarDealers.id == dealer_id,
+            CarDealers.deleted_at == None
+        ).first()
+
+        if dealer is None:
+            return
 
         for key, value in dealer_details.items():
             if value is not None:
@@ -127,8 +141,39 @@ class EditCarDealer(graphene.Mutation):
         return dealer
 
 
+class DeleteCar(graphene.Mutation):
+    class Arguments:
+        car_id = graphene.UUID(required=True)
+
+    Output = CarsGraphQLModel
+
+    @staticmethod
+    def mutate(parent, info, car_id):
+        car: Cars = db.query(Cars).filter(Cars.id == car_id).first()
+        car.deleted_at = datetime.datetime.now()
+        db.commit()
+        return
+
+
+class DeleteCarDealer(graphene.Mutation):
+    class Arguments:
+        dealer_id = graphene.UUID(required=True)
+
+    Output = CarDealersGraphQLModel
+
+    @staticmethod
+    def mutate(parent, info, dealer_id):
+        dealer: CarDealers = db.query(CarDealers).filter(
+            CarDealers.id == dealer_id).first()
+        dealer.deleted_at = datetime.datetime.now()
+        db.commit()
+        return
+
+
 class Mutation(graphene.ObjectType):
     create_car = CreateCar.Field()
     create_car_dealer = CreateCarDealer.Field()
     edit_car = EditCar.Field()
     edit_car_dealer = EditCarDealer.Field()
+    delete_car = DeleteCar.Field()
+    delete_dealer = DeleteCarDealer.Field()
