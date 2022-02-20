65c309e8d451416e0a0513848b88d251015966bd
fauster <federicoauster@gmail.com>
2022-02-20 02:57:14 -0300

    Created graphql route in main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 from pathlib import Path
 
+import graphene
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from starlette_graphene3 import GraphQLApp
 
 from src.routers import integration, synced, third_party
 from src.database.connection import Base, engine
+from src.schemas.query import Query, Mutation
 
 
 Base.metadata.create_all(bind=engine)
@@ -17,3 +20,7 @@ app = FastAPI()
 app.include_router(integration.router)
 app.include_router(synced.router)
 app.include_router(third_party.router)
+app.add_route(
+    '/graphql',
+    GraphQLApp(schema=graphene.Schema(query=Query, mutation=Mutation))
+)
